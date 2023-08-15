a9a3cdef8624c1ff986026bc8f2db324a7e02855
luzhixing12345 <luzhixing12345@163.com>
2023-08-15 15:07:59 +0800

    [fix] : use abosulte path

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 sudo apt install -y git vim build-essential tmux
 
+DOTFILE_PATH=$(pwd)
+
 # vim 配置
-ln -s .vimrc ~/.vimrc
+ln -s $DOTFILE_PATH/.vimrc ~/.vimrc
 mkdir -p ~/.vim/syntax
-cp c.vim ~/.vim/syntax/c.vim
+cp $DOTFILE_PATH/c.vim ~/.vim/syntax/c.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
     && vim -c 'source ~/.vimrc | PlugInstall' -c ':qall'
 
 # tmux 配置
-ln -s .tmux.conf ~/.tmux.conf
+ln -s $DOTFILE_PATH/.tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
 # docker
@@ -19,24 +21,13 @@ tmux source-file ~/.tmux.conf
 
 # bashrc
 #!/bin/bash
-
-# 获取当前路径下的 .clang-format 文件的路径
-clang_format_path=$(realpath .clang-format)
-
-# 检查 .clang-format 文件是否存在
-if [ -f "$clang_format_path" ]; then
-    # 将路径添加到文件中,如果文件不存在则创建
-    echo "export clangf=\"$clang_format_path\"" >> ".bashrc"
-else
-    echo ".clang-format file not found in the current directory."
-fi
-cat .bashrc >> ~/.bashrc
+cat $DOTFILE_PATH/.bashrc >> ~/.bashrc
 source ~/.bashrc
 
 # gdbinit
 mkdir -p ~/.config/gdb
-ln -s ./gdbinit ~/.config/gdb/gdbinit/
+ln -s $DOTFILE_PATH/gdbinit ~/.config/gdb/gdbinit/
 
 # ssh
-ssh-keygen -t rsa -C "luzhixing12345@163.com"
-cat ~/.ssh/id_rsa.pub
+# ssh-keygen -t rsa -C "luzhixing12345@163.com"
+# cat ~/.ssh/id_rsa.pub
